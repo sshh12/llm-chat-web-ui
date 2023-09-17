bc99b9a754d0e37e879964f074cb77f37dbfb27c
Shrivu Shankar <shrivu1122@gmail.com>
2023-09-17 14:23:20 -0700

    ugh

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  window.pwaInstall = event;
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
