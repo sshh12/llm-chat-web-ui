48d3ce970dc1928463226e9e43d85a41639148f6
Shrivu Shankar <shrivu1122@gmail.com>
2023-09-17 14:00:35 -0700

    tweak

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,10 +8,8 @@
 // service worker, and the Workbox build step will be skipped.
 
 import { clientsClaim } from "workbox-core";
-import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
 
 clientsClaim();
 
