a616a149af4ef8e82cae713aed818e98d5e257f7
Shrivu Shankar <shrivu1122@gmail.com>
2023-09-17 13:48:41 -0700

    tweaks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,3 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
